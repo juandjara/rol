be14e1dbba79b1848d7328a03c9b3ac9cda9614e
juandjara <juanorigami@gmail.com>
2015-10-19 19:53:43 +0200

    commiting from visual studio code

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
     var pre = $(this).text().split("\n");
     var lines = new Array(pre.length+1);
     for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
+      var wrap = Math.floor(pre[i].split("").length / 70);
+      if (pre[i] === "" && i == pre.length-1) {
         lines.splice(i, 1);
       } else {
         lines[i] = i+1;
@@ -23,14 +23,14 @@ $(document).ready(function(){
 
   var collectHeaders = function(){
     headings.push({"top":$(this).offset().top - 15,"text":$(this).text()});
-  }
+  };
 
-  if($(".markdown-body h1").length > 1) $(".markdown-body h1").each(collectHeaders)
-  else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders)
-  else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders)
+  if($(".markdown-body h1").length > 1)      $(".markdown-body h1").each(collectHeaders);
+  else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders);
+  else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders);
 
   $(window).scroll(function(){
-    if(headings.length==0) return true;
+    if(headings.length === 0) return true;
     var scrolltop = $(window).scrollTop() || 0;
     if(headings[0] && scrolltop < headings[0].top) {
       $(".current-section").css({"opacity":0,"visibility":"hidden"});
@@ -47,6 +47,6 @@ $(document).ready(function(){
   $(".current-section a").click(function(){
     $(window).scrollTop(0);
     return false;
-  })
+  });
 });
-})(jQuery)
+})(jQuery);
